9e12f448d1071e65db38ec2c6e81a8601ea1cf1f
stephen <stephenrainevilleta16@gmail.com>
2025-05-14 10:00:09 +0800

    Refactor MyApp: Remove unused theme configuration for improved code clarity

--- lib/main.dart
+++ lib/main.dart
@@ -12,9 +12,6 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       title: 'Wingr',
-      theme: ThemeData(
-        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
-      ),
       home: const LandingPage(),
     );
   }
